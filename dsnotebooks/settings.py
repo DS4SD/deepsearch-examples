e6d70784f5fed58c3a9783ee758fcebc9fa56888
Panos Vagenas <35837085+vagenas@users.noreply.github.com>
2024-03-05 14:03:47 +0100

    add generation params & skip-ingested flag
    Signed-off-by: Panos Vagenas <35837085+vagenas@users.noreply.github.com>

--- dsnotebooks/settings.py
+++ dsnotebooks/settings.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 from typing import Optional
 
+from deepsearch.cps.queries import ConstrainedWeight
 from dotenv import find_dotenv
 from pydantic import validator
 from pydantic_settings import BaseSettings
 
-from deepsearch.cps.queries import ConstrainedWeight
 
 class NotebookSettings(BaseSettings):
     class Config:
@@ -39,6 +39,8 @@ class CollQANotebookSettings(ProjectNotebookSettings):
     retr_k: int = 5
     text_weight: ConstrainedWeight = 0.1
     rerank: bool = False
+    skip_ingested_docs: bool = True
+
 
 class DocQANotebookSettings(CollQANotebookSettings):
     sem_on_idx_doc_hash: str
